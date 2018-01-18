c7ecf216039692dbc3da666bcd2b1baf3e971f28
Ola Vea <ola@olavea.com>
2018-01-18 11:57:23 +0100

    Update posts.js
    I redesigned the data model from @wesbos https://github.com/wesbos/Learn-Redux-Starter-Files
    
    By removing likes and comments from task #3 Design data model and create sample data

--- Client/data/posts.js
+++ Client/data/posts.js
@@ -1,36 +1,26 @@
 const posts = [
    {
       "code":"BAcyDyQwcXX",
-      "caption":"I am learning React.js and Redux with a video course by @wesbos called Learn Redux",
-      "likes":56,
       "id":"1161022966406956503",
       "display_src":"https://scontent-arn2-1.cdninstagram.com/vp/5e53d7473cd92d8162caab6bab1a4b79/5AF078AA/t51.2885-15/e35/26868214_1348431905268961_5798904889887162368_n.jpg"      
    },
    {
       "code":"BAcJeJrQca9",
-      "caption":"In the morning all crows are black. #crow #ITM",
-      "likes":59,
       "id":"1160844458347054781",
       "display_src":"https://scontent-arn2-1.cdninstagram.com/vp/c1cdf72b7d5736221b0f3c0ad5501dcc/5AEE887E/t51.2885-15/s640x640/sh0.08/e35/26072738_1778607539102181_4054382761589342208_n.jpg"
    },
    {
       "code":"BAF_KY4wcRY",
-      "caption":"Owl mum, tractor, car, owl baby og gull. Lillian Raae-Vea (3) told me what to draw.",
-      "likes":79,
       "id":"1154606670337393752",
       "display_src":"https://scontent-arn2-1.cdninstagram.com/vp/8a468e162cf84f1b93d1eb142fc01b46/5ADFE7B9/t51.2885-15/s640x640/sh0.08/e35/26156894_1956221098039360_3770277207604199424_n.jpg"
    },
    {
       "code":"BAPIPRjQce9",
-      "caption":"Seventh skiing trip og the season. #urbanskiing #skiing #ski #crosscountryskiing #rodeløkka #olanarr #tøyen",
-      "likes":16,
       "id":"1157179863266871229",
       "display_src":"https://scontent-arn2-1.cdninstagram.com/vp/2ce8ad84ba055da29d1e9869f1e5cefb/5AE1F13D/t51.2885-15/s640x640/sh0.08/e35/26070727_187378232004933_2783718950515507200_n.jpg"
    },
    {
       "code":"-hZh6IQcfN",
-      "caption":"First trip to Tryvann. Cross country skiing.",
-      "likes":22,
       "id":"1126293663140399053",
       "display_src":"https://scontent-arn2-1.cdninstagram.com/vp/38eafd7d5843610e91c2b16eb7baa4fe/5AE75871/t51.2885-15/s640x640/sh0.08/e35/26295729_156490118325358_1763657345767833600_n.jpg"
    }
